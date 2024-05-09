8532020c7f85a639b0e1235a38e3f0fb72a4bb07
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2024-05-09 20:28:23 +0000

    New model for product image is added and registered in admin along with the other two models, view and urls files also have been updated as required

--- products/models.py
+++ products/models.py
@@ -11,9 +11,26 @@ class Category(BaseModel):
 
 
 
+    def save(self , *args , **kwargs):
+        self.slug = slugify(self.category_name)
+        super(Category ,self).save(*args , **kwargs)
+
+
+    def __str__(self) -> str:
+        return self.category_name
+
+
+
 class Product(BaseModel):
     product_name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     price = models.IntegerField()
     product_description = models.TextField()
+
+
+
+class ProductImage(BaseModel):
+    product = models.ForeignKey(Product , on_delete=models.CASCADE , related_name="product_images")
+    image = models.ImageField(upload_to="product")
+
